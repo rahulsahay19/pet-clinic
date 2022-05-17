7bd4c6c3e49ab83ea6652f6885fb85d7abd80a6e
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-17 12:36:34 +0530

    Initial project

---
+++ src/main/java/myview/springframework/myviewpetclinic/MyviewPetClinicApplication.java
@@ -0,0 +1,13 @@
+package myview.springframework.myviewpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyviewPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyviewPetClinicApplication.class, args);
+    }
+
+}
