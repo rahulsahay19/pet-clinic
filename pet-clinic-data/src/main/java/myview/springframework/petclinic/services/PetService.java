f180018aa32c13a6f74dbee940559f1bb2a1b98e
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-19 13:29:32 +0530

    Implemented map based services

--- pet-clinic-data/src/main/java/myview/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/myview/springframework/petclinic/services/PetService.java
@@ -2,10 +2,6 @@ package myview.springframework.petclinic.services;
 
 import myview.springframework.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
---
+++ pet-clinic-data/src/main/java/myview/springframework/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package myview.springframework.petclinic.services.map;
+
+import myview.springframework.petclinic.model.Vet;
+import myview.springframework.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+
+}
