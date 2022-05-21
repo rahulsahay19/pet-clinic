8b425b0ec132863ca64dd267bbd5313bd2640a2f
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-21 13:01:54 +0530

    Made Root Level routing

---
+++ pet-clinic-web/src/main/java/myview/springframework/myviewpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package myview.springframework.myviewpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
