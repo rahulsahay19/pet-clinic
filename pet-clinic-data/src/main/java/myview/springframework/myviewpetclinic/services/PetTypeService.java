aaeffde76b7485b5c75391fd61d635c6163c9601
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-28 01:54:35 +0530

    Modified Data loader and complex save

---
+++ pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package myview.springframework.myviewpetclinic.services;
+
+import myview.springframework.myviewpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-web/src/main/java/myview/springframework/myviewpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/myview/springframework/myviewpetclinic/bootstrap/DataLoader.java
@@ -1,20 +1,27 @@
 package myview.springframework.myviewpetclinic.bootstrap;
 
 import myview.springframework.myviewpetclinic.model.Owner;
+import myview.springframework.myviewpetclinic.model.Pet;
+import myview.springframework.myviewpetclinic.model.PetType;
 import myview.springframework.myviewpetclinic.model.Vet;
 import myview.springframework.myviewpetclinic.services.OwnerService;
+import myview.springframework.myviewpetclinic.services.PetTypeService;
 import myview.springframework.myviewpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component //This wil become spring bean and will be available in spring context
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    private final PetTypeService petTypeService;
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -22,15 +29,39 @@ public class DataLoader implements CommandLineRunner {
         loadData();
     }
     private void loadData() {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
 
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Bangalore");
+        owner1.setCity("Bangalore");
+        owner1.setTelephone("123456789");
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("456 Delhi");
+        owner2.setCity("Delhi");
+        owner2.setTelephone("234567891");
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
         ownerService.save(owner2);
         System.out.println("Loaded owners");
 
