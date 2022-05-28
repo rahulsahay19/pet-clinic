6d57a6093cfdf897b70caf2bce00dafb2d42cc50
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-29 00:19:51 +0530

    Added test setup

---
+++ pet-clinic-web/src/test/java/myview/springframework/myviewpetclinic/MyviewPetClinicApplicationTest.java
@@ -0,0 +1,14 @@
+package myview.springframework.myviewpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+public class MyviewPetClinicApplicationTest {
+    @Test
+    public void contextLoads(){
+
+    }
+}
