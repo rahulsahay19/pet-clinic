3453e64222c5c0ed51981bc4cd3c1d51076daeed
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-28 19:40:57 +0530

    Added Lombok

--- pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/model/Speciality.java
@@ -1,18 +1,17 @@
 package myview.springframework.myviewpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="specialities")
 public class Speciality extends BaseEntity {
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/model/Vet.java
@@ -1,21 +1,20 @@
 package myview.springframework.myviewpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet extends Person{
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="vet_specialities", joinColumns = @JoinColumn(name="vet_id"), inverseJoinColumns = @JoinColumn(name = "specialities_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
