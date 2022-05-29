b13cff9ce110e090125918b5d58b19e8f3464d0f
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-29 19:53:22 +0530

    Added Controller views

--- pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/myview/springframework/myviewpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package myview.springframework.myviewpetclinic.services;
 
 import myview.springframework.myviewpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
